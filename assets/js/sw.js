eaba95f994cbb6636622d2b6d71c9006dba762ff
georgeee <george.agapov@gmail.com>
2021-09-22 12:00:56 +0300

    Update site

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.1.4--v13--sw/";
 const ASSETS_CACHE = "assets--v13--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2021-09-20T15:12:17+03:00--sw/";
+const CONTENT_CACHE = "content--2021-09-22T12:00:53+03:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
@@ -378,9 +378,6 @@ const PRE_CACHED_ASSETS = [
 const CONTENT_FILES = [
   "/",
   "/offline.html",
-  /**/"/LICENSE/",
-  /**/"/NOTICE/",
-  /**/"/CHANGELOG/",
   /**/
 ];
 
